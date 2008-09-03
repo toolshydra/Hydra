e77565138ded2f410017d2a792b49d738ced506f
Eduardo Valentin <eduardo.valentin@indt.org.br>
2008-09-03 13:49:53 -0400

    Add licence comment on src/smartenum.c
    Add licence comment on src/smartenum.c
    
    Signed-off-by: Eduardo Valentin <eduardo.valentin@indt.org.br>

--- src/smartenum.c
+++ src/smartenum.c
@@ -1,3 +1,13 @@
+/*
+ * src/smartenum.c
+ *
+ * Copyright (C) 2008 Eduardo Valentin <edubezval@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
