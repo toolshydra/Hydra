093063f6569bde575daaf20391704085d399e7a8
Eduardo Valentin <edubezval@gmail.com>
2010-01-20 19:23:28 +0200

    Aumentadas as fontes do grafico de configurações e traduzido para o portugues
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- scripts/plot_clustered_evaluated.sh
+++ scripts/plot_clustered_evaluated.sh
@@ -9,23 +9,23 @@ for i in $@ ; do
 PLOT="plot"
 for j in `seq 1 6` ; do
 	S=`expr 3 \* $j`
-	PLOT=$PLOT" \"$i\" using $S:$(expr $S + 1):xticlabel(1) title \" `expr $j + 3` tasks \","
+	PLOT=$PLOT" \"$i\" using $S:$(expr $S + 1):xticlabel(1) title \" `expr $j + 3`\","
 done
 PLOT=${PLOT%,}
 echo $PLOT
 
-$GNUPLOT << EOF
-set term post "CMR10"
-set term jpeg
+GDFONTPATH=/usr/share/fonts/truetype/latex-xft-fonts/ $GNUPLOT << EOF
+set term post
+set term jpeg  truecolor nocrop enhanced font "cmr10" 18
 set output "$i.jpg"
 set decimalsign ','
 set grid
-set xlabel "Applied prunings"
+set xlabel "Podas Aplicadas"
 set ylabel "R"
-set title "Evaluated Configurations Ratio"
-#set key outside bmargin
-set key box
-set yrange [1e-6:2]
+set title "Razão de Configurações Avaliadas"
+#set key outside
+set key center Left samplen 0.75 horizontal top box title "Quantidade de Tarefas"
+set yrange [1e-6:40]
 set logscale ycb
 
 #set format  y "%.2f"
@@ -33,7 +33,7 @@ set style data histogram
 set style histogram clustered errorbars gap 2 lw 2
 set xtics nomirror rotate by -45
 set style fill solid border -1
-set boxwidth 1
+#set boxwidth 10
 $PLOT
 set output
 set terminal pop
