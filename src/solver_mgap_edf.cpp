53d2f094bd99289d95d9e1190903229c694b7bcf
Eduardo Valentin <Eduardo.Valentin@icomp.ufam.edu.br>
2015-06-09 20:49:39 -0700

    solvers_edf: add LCM computation and energy consumption
    Introduce energy consumption computation based on
    tasks LCM (active vs. inactive time).
    
    Signed-off-by: Eduardo Valentin <Eduardo.Valentin@icomp.ufam.edu.br>

--- src/solver_mgap_edf.cpp
+++ src/solver_mgap_edf.cpp
@@ -1,17 +1,87 @@
 #include <ilcplex/ilocplex.h>
+#include <gcd_hash.h>
 
 ILOSTLBEGIN
 
+unsigned long long gcd(unsigned long long a, unsigned long long b)
+{
+	long long tmp;
+
+	/* As simple as Euclides told me */
+	while (b != 0) {
+
+		tmp = b;
+		b = a % b;
+		a = tmp;
+	}
+
+	return a;
+}
+
+unsigned long long gcd_hash(unsigned long long i, unsigned long long j)
+{
+	unsigned long long a, b;
+
+	a = i; b = j;
+
+	if ((a < MAX_GCD) && (b < MAX_GCD)) {
+		return gcd_lookup[a][b];
+	}
+	if ((a % 2) == 0 && (b % 2) == 0) { /* both even*/
+		a = a >> 1; b = b >> 1;
+		if ((a < MAX_GCD) && (b < MAX_GCD))
+			return gcd_lookup[a][b] * 2;
+	} else if ((a % 2) == 1 && (b % 2) == 0) {
+		b = b >> 1;
+		if ((a < MAX_GCD) && (b < MAX_GCD))
+			return gcd_lookup[a][b];
+	} else if ((a % 2) == 0 && (b % 2) == 1) {
+		a = a >> 1;
+		if ((a < MAX_GCD) && (b < MAX_GCD))
+			return gcd_lookup[a][b];
+	}
+
+	return gcd(i, j);
+}
+
+long long lcm(long long a, long long b)
+{
+	long long tmp = gcd_hash(a, b);
+
+	if (tmp)
+		return (a * b) / tmp;
+
+	return 0;
+}
+
+long long computeLCM(IloNumArray periods)
+{
+	int j, k;
+	long long LCM;
+
+	LCM = 0;
+	for (j = 0; j < periods.getSize(); j++) {
+		long long period = periods[j];
+
+		if (LCM == 0)
+			LCM = period;
+
+		LCM = lcm(LCM, period);
+	}
+
+	return LCM;
+}
 
 int main(int argc, char **argv)
 {
 	IloEnv env;
 	try {
 		IloInt i, j, k;
-		double alpha;
+		double alpha, bound;
 		int nAgents, nTasks, nLevels;
 		IloNumArray2 cycles(env), voltage(env), frequency(env);
 		IloNumArray period(env);
+		long long LCM;
 
 		const char* filename  = "mgap-edf.dat";
 		if (argc > 1)
@@ -24,11 +94,13 @@ int main(int argc, char **argv)
 			throw(-1);
 		}
 		/* float array(nTasks) matrix(nAgents x nTasks) matrix(nAgents x nLevels) matrix(nAgents x nLevels) */
-		file >> alpha >> period >> cycles >> voltage >> frequency;
+		file >> alpha >> bound >> period >> cycles >> voltage >> frequency;
 
 		nAgents = cycles.getSize();
 		nTasks = period.getSize();
 		nLevels = frequency[0].getSize();
+
+		LCM = computeLCM(period);
 
 		IloArray<IloArray<IloNumArray> > cost(env, nAgents);
 		IloArray<IloArray<IloNumArray> > req(env, nAgents);
@@ -44,7 +116,7 @@ int main(int argc, char **argv)
 		for(i = 0; i < nAgents; i++) {
 			for(j = 0; j < nTasks; j++) {
 				for(k = 0; k < nLevels; k++) {
-					cost[i][j][k] = alpha * cycles[i][j] *
+					cost[i][j][k] = alpha * (LCM / period[j]) * cycles[i][j] *
 							(voltage[i][k] * voltage[i][k]);
 					req[i][j][k] = cycles[i][j] / (frequency[i][k] * period[j]);
 				}
@@ -73,7 +145,7 @@ int main(int argc, char **argv)
 				for(k = 0; k < nLevels; k++)
 					v += req[i][j][k] * x[i][j][k];
 			}
-			model.add(v <= 1); /* Each agent has a budget */
+			model.add(v <= bound); /* Each agent has a budget */
 			v.end();
 		}
 
